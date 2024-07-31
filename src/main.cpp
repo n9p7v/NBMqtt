d45f2394d2fcd91a47b4a4199b93b4ebfaaaf93a
Nicholas Dimitriou <dimitriounicholas@gmail.com>
2024-07-31 10:12:44 +0300

    Impement SerialSARAPassthrough sketch

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,31 @@
 #include <Arduino.h>
+#include "NBMqtt.h"
+
+NBMqtt mqtt(true);
 
 void setup() {
+ // NEVER EVER use RESET_N
+  pinMode(SARA_RESETN, OUTPUT);
+  digitalWrite(SARA_RESETN, LOW);
 
+  // Send Poweron pulse
+  pinMode(SARA_PWR_ON, OUTPUT);
+  digitalWrite(SARA_PWR_ON, HIGH);
+  delay(150);
+  digitalWrite(SARA_PWR_ON, LOW);
+
+  Serial.begin(115200);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB
+  }
+  SerialSARA.begin(115200);
 }
 
 void loop() {
-
+ if (Serial.available()) {
+    SerialSARA.write(Serial.read());
+  }
+  if (SerialSARA.available()) {
+    Serial.write(SerialSARA.read());
+  }
 }
