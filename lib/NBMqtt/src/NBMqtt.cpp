cdeb41f39b60128c2a5ecca343b2982a3b5a6e45
Nicholas Dimitriou <dimitriounicholas@gmail.com>
2024-07-31 10:07:08 +0300

    Add NBMqtt library header and source files.

---
+++ lib/NBMqtt/src/NBMqtt.cpp
@@ -0,0 +1,209 @@
+#include "NBMqtt.h"
+
+NBMqtt::NBMqtt(bool debug) {
+  if (debug) {
+    MODEM.debug();
+  }
+}
+
+NBMqtt::~NBMqtt() {}
+
+/********** MQTT profile configuration +UMQTT **********/
+
+bool NBMqtt::setMQTTClientID(const char *client_id) {
+  String response;
+  char client_id_truncated[129];
+  strncpy(client_id_truncated, client_id, sizeof(client_id_truncated) - 1);
+  client_id_truncated[sizeof(client_id_truncated) - 1] = '\0';
+  MODEM.sendf("AT+UMQTT=0,\"%s\"", client_id_truncated);
+  if (MODEM.waitForResponse(100, &response) == 1) {
+    return response.startsWith("+UMQTT: 0,1");
+  } else {
+    return false;
+  }
+}
+
+bool NBMqtt::setMQTTPort(uint16_t local_port) {
+  String response;
+  MODEM.sendf("AT+UMQTT=1,%u", local_port);
+  if (MODEM.waitForResponse(100, &response) == 1) {
+    return response.startsWith("+UMQTT: 1,1");
+  } else {
+    return false;
+  }
+}
+
+bool NBMqtt::setMQTTServerName(const char *server_name) {
+  String response;
+  char server_name_truncated[129];
+  strncpy(server_name_truncated, server_name, sizeof(server_name_truncated) - 1);
+  server_name_truncated[sizeof(server_name_truncated) - 1] = '\0';
+  MODEM.sendf("AT+UMQTT=2,\"%s\"", server_name_truncated);
+  if (MODEM.waitForResponse(100, &response) == 1) {
+    return response.startsWith("+UMQTT: 2,1");
+  } else {
+    return false;
+  }
+}
+
+bool NBMqtt::setMQTTServerIP(const char *IP_address, uint16_t local_port) {
+  String response;
+  MODEM.sendf("AT+UMQTT=3,\"%s\",%u", IP_address, local_port);
+  if (MODEM.waitForResponse(100, &response) == 1) {
+    return response.startsWith("+UMQTT: 3,1");
+  } else {
+    return false;
+  }
+}
+
+bool NBMqtt::setMQTTUsernamePassword(const char *username, const char *password) {
+  String response;
+  char username_truncated[31];
+  char password_truncated[31];
+  strncpy(username_truncated, username, sizeof(username_truncated) - 1);
+  username_truncated[sizeof(username_truncated) - 1] = '\0';
+  strncpy(password_truncated, password, sizeof(password_truncated) - 1);
+  password_truncated[sizeof(password_truncated) - 1] = '\0';
+  MODEM.sendf("AT+UMQTT=4,\"%s\",\"%s\"", username_truncated, password_truncated);
+  if (MODEM.waitForResponse(100, &response) == 1) {
+    return response.startsWith("+UMQTT: 4,1");
+  } else {
+    return false;
+  }
+}
+
+bool NBMqtt::setMQTTKeepAlive(uint16_t keep_alive) {
+  String response;
+  MODEM.sendf("AT+UMQTT=10,%u", keep_alive);
+  if (MODEM.waitForResponse(100, &response) == 1) {
+    return response.startsWith("+UMQTT: 10,1");
+  } else {
+    return false;
+  }
+}
+
+bool NBMqtt::setMQTTsecure(bool MQTT_secure, uint8_t USECMNG_profile) {
+  String response;
+  MODEM.sendf("AT+UMQTT=11,%u,%u", MQTT_secure, USECMNG_profile);
+  if (MODEM.waitForResponse(100, &response) == 1) {
+    return response.startsWith("+UMQTT: 11,1");
+  } else {
+    return false;
+  }
+}
+
+bool NBMqtt::setCommonMQTTProfile(const char *client_id,
+                            uint16_t local_port,
+                            const char *IP_address,
+                            uint16_t keep_alive,
+                            const char *username,
+                            const char *password,
+                            bool MQTT_secure,
+                            uint8_t USECMNG_profile) {
+  bool result = true;
+  result &= setMQTTClientID(client_id);
+  result &= setMQTTPort(local_port);
+  result &= setMQTTServerIP(IP_address, local_port);
+  if (username && password) {
+    result &= setMQTTUsernamePassword(username, password);
+  }
+  result &= setMQTTKeepAlive(keep_alive);
+  if (MQTT_secure) {
+    result &= setMQTTsecure(MQTT_secure, USECMNG_profile);
+  }
+  return result;
+}
+
+bool NBMqtt::setMQTTProfile(const char *client_id,
+                            uint16_t local_port,
+                            const char *IP_address,
+                            uint16_t keep_alive) {
+  return setCommonMQTTProfile(client_id, local_port, IP_address, keep_alive, nullptr, nullptr, false, 0);
+}
+
+bool NBMqtt::setMQTTProfileAuth(const char *client_id,
+                                uint16_t local_port,
+                                const char *IP_address,
+                                const char *username,
+                                const char *password,
+                                uint16_t keep_alive) {
+  return setCommonMQTTProfile(client_id, local_port, IP_address, keep_alive, username, password, false, 0);
+}
+
+bool NBMqtt::setMQTTProfileSecure(const char *client_id,
+                                  uint16_t local_port,
+                                  const char *IP_address,
+                                  uint16_t keep_alive,
+                                  bool MQTT_secure,
+                                  uint8_t USECMNG_profile) {
+  return setCommonMQTTProfile(client_id, local_port, IP_address, keep_alive, nullptr, nullptr, MQTT_secure, USECMNG_profile);
+}
+
+/********** TODO: MQTT will topic configuration +UMQTTWTOPIC **********/
+
+/********** TODO: MQTT will message configuration +UMQTTWMSG **********/
+
+/********** Save/Restore MQTT profile from NVM +UMQTTNV **********/
+
+bool NBMqtt::setMQTTNvmMode(uint8_t NVM_mode) {
+  String response;
+  MODEM.sendf("AT+UMQTTNV=%u", NVM_mode);
+  if (MODEM.waitForResponse(100, &response) == 1) {
+    String expected_response = "+UMQTTNV: " + String(NVM_mode) + ",1";
+    return response.startsWith(expected_response);
+  } else {
+    return false;
+  }
+}
+
+/********** MQTT command +UMQTTC **********/
+
+bool NBMqtt::logoutMQTTClient(void) {
+  String response;
+  MODEM.sendf("AT+UMQTTC=0");
+  if (MODEM.waitForResponse(100, &response) == 1) {
+    return response.startsWith("+UMQTTC: 0,1");
+  } else {
+    return false;
+  }
+}
+
+bool NBMqtt::loginMQTTClient(void) {
+  String response;
+  MODEM.sendf("AT+UMQTTC=1");
+  if (MODEM.waitForResponse(100, &response) == 1) {
+    return response.startsWith("+UMQTTC: 1,1");
+  } else {
+    return false;
+  }
+}
+
+bool NBMqtt::publishMQTTTopic(uint8_t QoS, bool retain, const char *topic_name, char *message) {
+  String response;
+  MODEM.sendf("AT+UMQTTC=2,%u,%d,\"%s\",\"%s\"", QoS, retain, topic_name, message);
+  if (MODEM.waitForResponse(100, &response) == 1) {
+    return response.startsWith("+UMQTTC: 2,1");
+  } else {
+    return false;
+  }
+}
+
+// TODO: MQTT publish a file to a topic
+
+// TODO: MQTT subscribe to the specified topic filter
+
+// TODO: MQTT unsubscribe from the specified topic filter
+
+// TODO: MQTT read message
+
+// TODO: MQTT verbose/terse Reception format
+
+bool NBMqtt::pingMQTTServer(const char *IP_address) {
+  String response;
+  MODEM.sendf("AT+UMQTTC=8,\"%s\"", IP_address);
+  if (MODEM.waitForResponse(100, &response) == 1) {
+    return response.startsWith("+UMQTTC: 8,1");
+  } else {
+    return false;
+  }
+}
---
+++ lib/NBMqtt/src/NBMqtt.h
@@ -0,0 +1,218 @@
+/*
+ * This file is an extention of the MKR NB library.
+ * Copyright (c) 2024 Nicholas Dimitriou. All rights reserved.
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef _NBMQTT_H
+#define _NBMQTT_H
+
+#include "Modem.h"
+// #include "NB.h"
+
+class NBMqtt {
+
+public:
+  /**
+   * Constructor
+   * 
+   * @param debug - if true, dumps all AT dialogue to Serial
+   * @return -
+   */
+  NBMqtt(bool debug = false);
+
+  /**
+   * Deconstructor
+   */
+  ~NBMqtt();
+
+  /**
+   * Set MQTT client ID with a limit of 128 characters
+   * 
+   * @return - 1 = OK
+   * @return - 0 = ERROR
+   */
+  bool setMQTTClientID(const char *client_id);
+
+  /**
+   * Set MQTT port number
+   * 
+   * @param 1883 non-TLS MQTT
+   * @param 8883 TLS MQTT
+   * @return - 1 = OK
+   * @return - 0 = ERROR
+   */
+  bool setMQTTPort(uint16_t local_port);
+
+  /**
+   * Set MQTT server name with a limit of 128 characters
+   * 
+   * @return - 1 = OK
+   * @return - 0 = ERROR
+   */
+  bool setMQTTServerName(const char *server_name);
+
+  /**
+   * Set MQTT server IP and port number
+   * 
+   * @param IP IPv4
+   * @param 1883 non-TLS MQTT
+   * @param 8883 TLS MQTT
+   * @return - 1 = OK
+   * @return - 0 = ERROR
+   */
+  bool setMQTTServerIP(const char *IP_address, uint16_t local_port);
+
+  /**
+   * Set MQTT username and password with a limit of 30 characters each
+   * 
+   * @return - 1 = OK
+   * @return - 0 = ERROR
+   */
+  bool setMQTTUsernamePassword(const char *username, const char *password);
+  
+  /**
+   * Set MQTT server keep alive time in seconds
+   * 
+   * @param 0 minimum (default)
+   * @param 65535 maximum
+   * @return - 1 = OK
+   * @return - 0 = ERROR
+   */
+  bool setMQTTKeepAlive(uint16_t keep_alive);
+
+  /**
+   * Set MQTT secure option
+   * 
+   * @param 0 no-TLS MQTT (default)
+   * @param 1 TLS MQTT
+   * @return - 1 = OK
+   * @return - 0 = ERROR
+   */
+  bool setMQTTsecure(bool MQTT_secure, uint8_t USECMNG_profile);
+
+  /**
+   * Set MQTT basic profile
+
+   */
+  bool setCommonMQTTProfile(const char *client_id,
+                            uint16_t local_port,
+                            const char *IP_address,
+                            uint16_t keep_alive,
+                            const char *username,
+                            const char *password,
+                            bool MQTT_secure,
+                            uint8_t USECMNG_profile);
+  
+  /**
+   * Set MQTT basic profile
+   * 
+   * @param cliend_id
+   * @param local_port: 1883 no-TLS
+   * @param IP_adress
+   * @param keep_alive: 0 (default) - 65535
+   * 
+   * @return - 1 = OK
+   * @return - 0 = ERROR
+   */
+  bool setMQTTProfile(const char *client_id,
+                      uint16_t local_port,
+                      const char *IP_address,
+                      uint16_t keep_alive);
+
+  /**
+   * Set MQTT Auth profile
+   * 
+   * @param cliend_id
+   * @param local_port: 1883 no-TLS
+   * @param IP_adress
+   * @param username
+   * @param password
+   * @param keep_alive: 0 (default) - 65535
+   * 
+   * @return - 1 = OK
+   * @return - 0 = ERROR
+   */
+  bool setMQTTProfileAuth(const char *client_id,
+                          uint16_t local_port,
+                          const char *IP_address,
+                          const char *username,
+                          const char *password,
+                          uint16_t keep_alive);
+
+  /**
+   * Set MQTT TLS profile
+   * 
+   * @param cliend_id
+   * @param local_port: 8883 TLS
+   * @param IP_adress
+   * @param keep_alive: 0 (default) - 65535
+   * @param MQTT_secure: true
+   * @param USECMNG_profile
+   * 
+   * @return - 1 = OK
+   * @return - 0 = ERROR
+   */
+  bool setMQTTProfileSecure(const char *client_id,
+                      uint16_t local_port,
+                      const char *IP_address,
+                      uint16_t keep_alive,
+                      bool MQTT_secure = true,
+                      uint8_t USECMNG_profile = 0);
+
+  /**
+   * Save/Restore MQTT profile to/from non-volatile memory (NVM)
+   * 
+   * @param 0 restore default
+   * @param 1 read profile
+   * @param 2 save profile
+   * @return - 1 = OK
+   * @return - 0 = ERROR
+   */
+  bool setMQTTNvmMode(uint8_t NVM_mode);
+
+  /**
+   * Disconnect from MQTT server
+   */
+  bool logoutMQTTClient(void);
+
+  /**
+   * Connect to MQTT server
+   */
+  bool loginMQTTClient(void);
+
+  /**
+   * MQTT publish to a topic
+   * 
+   * @param QoS 0 (default) at most once delivery
+   * @param QoS 1 at least once delivery
+   * @param QoS 2 exactly once delivery
+   * @param retain 0 (default) the message will not be retained by the MQTT broker
+   * @param retain 1 the message will be retained by the MQTT broker
+   * @param topic_name MQTT topic name to publish to
+   * @param message Message for publishing
+   */
+  bool publishMQTTTopic(uint8_t QoS, bool retain, const char *topic_name, char *message);
+
+  /**
+   * Issue a ping to MQTT server
+   * 
+   * @param IP_address MQTT server IP
+   */
+  bool pingMQTTServer(const char *IP_address);
+};
+
+#endif // _NBMQTT_H
