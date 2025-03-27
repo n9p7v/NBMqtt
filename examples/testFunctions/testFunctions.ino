b5ef91812d4be6289fa45c4da136eb364169608d
Nicholas Dimitriou <dimitriounicholas@gmail.com>
2025-03-27 20:22:42 +0200

    Updated example.

--- examples/testFunctions/testFunctions.ino
+++ examples/testFunctions/testFunctions.ino
@@ -1,8 +1,15 @@
 #include <MKRNB.h>
 #include "NBMqtt.h"
+#include "arduino_secrets.h"
 
+// Please enter your sensitive data in the Secret tab or arduino_secrets.h
+// PIN Number
+const char PINNUMBER[] = SECRET_PINNUMBER;
+
+#define DELAY_TIME 30000
 #define CLIENT_ID "clientId"
-#define NVM_MODE 1
+#define NVM_SAVE 2
+#define NVM_RESTORE 1
 #define MQTT_PORT 1883
 #define SERVER_IP "127.0.0.1"
 #define KEEP_ALIVE 0
@@ -11,9 +18,12 @@
 #define QOS 0
 #define RETAIN 0
 #define TOPIC "test"
-#define PAYLOAD "{\"key\":\"value\"}"
 
-NBMqtt mqttClient(true); // true enables debugging
+// Initialize library instance
+NBClient client;
+GPRS gprs;
+NB nbAccess;
+NBMqtt mqttClient();
 
 void setup() {
   // NEVER EVER use RESET_N
@@ -26,33 +36,57 @@ void setup() {
   delay(150);
   digitalWrite(SARA_PWR_ON, LOW);
 
+  // Initialize serial communications and wait for port to open:
   Serial.begin(115200);
   while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB
+    ; // wait for serial port to connect. Needed for native USB port only
   }
   SerialSARA.begin(115200);
-  
 
-  // Set MQTT profile
-  mqttClient.setMQTTProfileAuth(CLIENT_ID,MQTT_PORT,SERVER_IP,USERNAME,PASSWORD,0);
+  Serial.println("Starting Arduino web client.");
+  // Connection state
+  boolean connected = false;
 
-  // Connect to MQTT broker
-  mqttClient.loginMQTTClient();
-  bool status = false;
-  while (!status) {
-    // Publish a payload to a topic
-    status = mqttClient.publishMQTTTopic(QOS,RETAIN,TOPIC,PAYLOAD);
+  // After starting the modem with NB.begin() attach to the GPRS network with the APN, login and password
+  while (!connected) {
+    if ((nbAccess.begin(PINNUMBER) == NB_READY) &&
+        (gprs.attachGPRS() == GPRS_READY)) {
+      connected = true;
+
+      // Set MQTT profile
+      Serial.println("Configuring MQTT...");
+      mqttClient.setMQTTClientID(CLIENT_ID);
+      mqttClient.setMQTTPort(MQTT_PORT);
+      mqttClient.setMQTTServerIP(SERVER_IP, MQTT_PORT);
+      mqttClient.setMQTTUsernamePassword(USERNAME, PASSWORD);
+      mqttClient.setMQTTKeepAlive(KEEP_ALIVE);
+
+      // Save profile to memmory
+      mqttClient.setMQTTNvmMode(NVM_SAVE);
+
+    } else {
+      Serial.println("Not connected");
+      delay(1000);
+    }
   }
-  // Disconnect from MQTT broker
-  mqttClient.logoutMQTTClient();
 }
 
-void loop() {  
-  if (Serial.available()) {
-    SerialSARA.write(Serial.read());
-  }
+void loop() {
+  // Restore proofile from memmory
+  mqttClient.setMQTTNvmMode(NVM_RESTORE);
 
-  if (SerialSARA.available()) {
-    Serial.write(SerialSARA.read());
-  }
+  // Connecting to MQTT broker
+  Serial.println("Connecting...");
+  mqttClient.loginMQTTClient();
+  
+  // Publish a payload to a topic
+  Serial.println("Publish payload...");
+  char payload[];
+  status = mqttClient.publishMQTTTopic(QOS,RETAIN,TOPIC,payload);
+  
+  // Disconnecting from MQTT broker
+  Serial.println("Disconnecting...");
+  mqttClient.logoutMQTTClient();
+
+  delay(DELAY_TIME);
 }
